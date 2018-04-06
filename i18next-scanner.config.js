85df819c7dda91e753a9455a423e2f33be111b50
littekite <15829705867@sina.cn>
2018-04-06 11:07:39 +0800

    增加多语言

---
+++ i18next-scanner.config.js
@@ -0,0 +1,31 @@
+// This configures i18next-scanner to extract every translation to i18n/catalog.json
+module.exports = {
+  options: {
+    debug: true,
+    func: {
+      list: ['t'],
+      extensions: ['.js', '.jsx'],
+    },
+    lngs: ['en','zh'],
+    ns: ['translation'],
+    defaultLng: 'zh',
+    defaultNs: 'translation',
+    defaultValue: (lng, ns, key) => {
+      if (lng === 'zh') {
+        // Return key as the default value for English language
+        return key;
+      }
+
+      // Return the string '__NOT_TRANSLATED__' for other languages
+      return '__NOT_TRANSLATED__';
+    },
+    resource: {
+      savePath: 'i18n/catalog.json',
+      jsonIndent: 2,
+      lineEnding: '\n',
+    },
+    nsSeparator: false,
+    keySeparator: false,
+    removeUnusedKeys: true,
+  },
+};
